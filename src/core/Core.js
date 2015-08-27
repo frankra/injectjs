3d182eac38772077e2e027e7395c639db9a62ed2
Rafael <franktestrlz@gmail.com>
2015-08-26 23:14:53 -0300

    [FEATURE] Added bootstrap script
     - This script should be used to bootstrap the framework.
       The idea is that on the app.js file, the consumer should call it like:
       require('node-injectjs')(sProjectPath), so the framework would setup
       itself based on the given path.

--- src/core/Core.js
+++ src/core/Core.js
@@ -1,16 +1,17 @@
+module.exports = function(sPath){
+	//Load test dependencies
+	require(sPath + '/src/core/Utils.js')();
 
-//Configure base
-__base ? global.__base = 'c:/Users/i834660/GIT/2stepauth-web/' : null;
+	require(sPath + '/src/core/Import.js');
+		//Configure module path
+	Import.mapModulePath('src.core','/src/core');
 
-//Setup Node dependency manager
-var fnNodeDependencyManager = require(__base + '/backend/src/foundation/core/NodeDependencyManager.js');
-require(__base + '/backend/src/foundation/core/Import.js');
-require(__base + '/backend/src/foundation/core/Utils.js')();
-global.NodeDependencyManager = new fnNodeDependencyManager({
-		path:'backend/src/foundation/core/NodeDependenciesMap.json'
+	//Setup Node dependency manager
+	var fnNodeDependencyManager = require(sPath + '/src/core/NodeDependencyManager.js');
+	global.NodeDependencyManager = new fnNodeDependencyManager({
+		path: sPath + '/node_dependencies.config.json'
 	});
-//Configure module path
-Import.mapModulePath('backend','backend');
 
-//Start define
-global.define = require(__base + '/backend/src/foundation/core/define.js')(NodeDependencyManager,Import);
+	//Start define
+	global.define = require(sPath + '/src/core/define.js')(NodeDependencyManager,Import);
+}
