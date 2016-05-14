de2efc246d170dbfe328e4cff08d4759add6b595
Rafael Frank <rafael.frank@sap.com>
2016-05-14 20:04:53 -0300

    Added JSDocs for NodeDependencyManager

--- src/core/NodeDependencyManager.js
+++ src/core/NodeDependencyManager.js
@@ -1,6 +1,51 @@
 (function(){
 	"use strict";
 
+	/**
+	* Node dependencies resolver. This module reads the configuration defined on the 'node_dependencies.config.json' file and resolve
+	* the dependencies when required through the `define` api.
+	* The node dependencies must to be defined with a `$` in front of it.
+	* @example
+	* // Given that the 'node_dependencies.config.json' looks like:
+	* {
+	*   "Express": {
+	*     "name" : "express",
+	*     "execute" : false
+	*   },
+	*   "App": {
+	*	    "name" : "express",
+	*	    "execute" : true
+	*   },
+	*   "HTTP": {
+	*     "name" : "http",
+	*     "execute" : true,
+	*     "executionPath" : "Server",
+	*     "executionArguments" : [{
+	*       "alias" : "App"
+	*     }]
+	*   }
+	* }
+	* // Then the `define` syntax will look like:
+	* module.exports = function(fnResolve){
+	*   "use strict";
+	*   define(['$App','$Express','$HTTP'],function(App, Express, HTTP){
+	*     App.use(Express.static(process.cwd()));
+	*
+	*     var portNumber = 3000;
+	*
+	*     HTTP.listen(portNumber, function(){
+	*       console.log('listening on localhost: ' + portNumber);
+	*     });
+	*
+	*     fnResolve(App) //Resolve the promise once this module is ready.
+	*   });
+	* }
+	* @class injectjs.core.NodeDependencyManager
+	* @param {Object} mParameters
+	*		The configuration object.
+	*	@param {String} mParameters.path
+	*		The path of where the NodeDependencyManager should look for the `node_dependencies.config.json` file
+	*/
 	function NodeDependencyManager(mParameters){
 		if (!mParameters){
 			throw new Error('Missing "mParameters" argument to initialize NodeDependencyManager');
@@ -15,11 +60,24 @@
 
 		return this;
 	}
-
+	/**
+	* Returns the contents of the 'node_dependencies.config.json'
+	* @return {Object} mConfig
+	*		The configuration file contents.
+	* @public
+	* @memberOf injectjs.core.NodeDependencyManager
+	*/
 	NodeDependencyManager.prototype.getConfig = function(){
 		return this._mConfig;
 	};
-
+	/**
+	* Fetches the required node dependency by its alias.
+	* The alias has to be defined on the 'node_dependencies.config.json' file.
+	* @return {Promise} oModulePromise
+	*		The Promise to be resolved once the module is loaded
+	* @public
+	* @memberOf injectjs.core.NodeDependencyManager
+	*/
 	NodeDependencyManager.prototype.getDependency = function(sAlias){
 		if (this._mConfig.hasOwnProperty(sAlias)){
 			return new Promise(injectjs.core.Utils.proxy(function(fnResolve){
